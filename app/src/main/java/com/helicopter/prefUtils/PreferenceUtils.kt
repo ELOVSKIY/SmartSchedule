d59c9c0b254b431c2aa1a79976c9428a22fcb118
Helicoptera <stas.elovsky@gmail.com>
2020-02-27 17:47:39 +0200

    update pref utils

--- app/src/main/java/com/helicopter/prefUtils/PreferenceUtils.kt
+++ app/src/main/java/com/helicopter/prefUtils/PreferenceUtils.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 
 private const val WEEK_NUMB_PREFERENCE = "WEEK_NUMB_PREFERENCE"
-
 private const val WEEK_NUMB_KEY = "WEEK_NUMB_KEY"
 private const val WEEK_NUMB_DEFAULT = 1
 
@@ -19,4 +18,22 @@ fun saveWeekNumb(weekNumb: Int, app: Application){
 fun fetchWeekNumb(app: Application): Int{
     val preference = app.getSharedPreferences(WEEK_NUMB_PREFERENCE, Context.MODE_PRIVATE)
     return preference.getInt(WEEK_NUMB_KEY, WEEK_NUMB_DEFAULT)
+}
+
+
+private const val LAST_UPDATE_TIME_PREFERENCE = "LAST_UPDATE_TIME_PREFERENCE"
+private const val LAST_UPDATE_TIME_KEY = "LAST_UPDATE_TIME"
+private const val LAST_UPDATE_TIME_DEFAULT = 0
+
+fun saveLastUpdateTime(lastUpdateTime: Int, app: Application){
+    val preference = app.getSharedPreferences(LAST_UPDATE_TIME_PREFERENCE, Context.MODE_PRIVATE)
+    with(preference.edit()){
+        putInt(LAST_UPDATE_TIME_KEY, lastUpdateTime)
+        apply()
+    }
+}
+
+fun fetchLastUpdateTime(app: Application): Int{
+    val preference = app.getSharedPreferences(LAST_UPDATE_TIME_PREFERENCE, Context.MODE_PRIVATE)
+    return preference.getInt(LAST_UPDATE_TIME_KEY, LAST_UPDATE_TIME_DEFAULT)
 }
