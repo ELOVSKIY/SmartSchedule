99c8e35a14a56006849ea71088195eab38e7b7d4
Stanislav <stas.elovsky@gmail.com>
2020-02-28 12:17:38 +0200

    trouble with pager

--- app/src/main/java/com/helicopter/ui/fragments/list/ListHolderFragment.kt
+++ app/src/main/java/com/helicopter/ui/fragments/list/ListHolderFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.helicopter.R
 
 import com.helicopter.databinding.ListHolderFragmentBinding
 import com.helicopter.ui.adapter.pages.ListPagesAdapter
+import com.helicopter.ui.fragments.ObservableFragment
 
-class ListHolderFragment : Fragment() {
+class ListHolderFragment : ObservableFragment() {
 
     private lateinit var viewModel: ListHolderViewModel
     private lateinit var binding: ListHolderFragmentBinding
@@ -26,9 +30,21 @@ class ListHolderFragment : Fragment() {
         val listPagesAdapter = ListPagesAdapter(childFragmentManager, activity!!.application)
         binding.pages.adapter = listPagesAdapter
         binding.listTabs.setupWithViewPager(binding.pages)
-
-
         return binding.root
     }
 
+    override fun setObservers() {
+        viewModel.navigateToSelect.observe(viewLifecycleOwner, Observer{
+            viewModel.onNavigateToSelect()
+            val navController = view!!.findNavController()
+            when (binding.pages.currentItem){
+                0 -> {
+                    navController.navigate(R.id.action_ListHolderFragment_to_SelectGroupFragment)
+                }
+                1 ->{
+                    navController.navigate(R.id.action_ListHolderFragment_to_SelectEmployeeFragment)
+                }
+            }
+        })
+    }
 }
--- app/src/main/java/com/helicopter/ui/fragments/list/ListHolderViewModel.kt
+++ app/src/main/java/com/helicopter/ui/fragments/list/ListHolderViewModel.kt
@@ -1,7 +1,20 @@
 package com.helicopter.ui.fragments.list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ListHolderViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _navigateToSelect = MutableLiveData<Boolean>()
+    val navigateToSelect: LiveData<Boolean>
+        get() = _navigateToSelect
+
+    fun onFabClick(){
+        _navigateToSelect.value = true
+    }
+
+    fun onNavigateToSelect(){
+        _navigateToSelect.value = false
+    }
 }
