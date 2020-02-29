36ad476cca5a04e9a0403e862fd77ea47d94d651
Stanislav <stas.elovsky@gmail.com>
2020-02-29 03:13:12 +0200

    employee item xml

--- app/src/main/java/com/helicopter/ui/bindingUtils/ImageViewUtils.kt
+++ app/src/main/java/com/helicopter/ui/bindingUtils/ImageViewUtils.kt
@@ -2,11 +2,14 @@ package com.helicopter.ui.bindingUtils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.helicopter.R
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("url")
-fun ImageView.setEmployeeImage(url: String){
+fun ImageView.setEmployeeImage(url: String?){
     Picasso.get()
         .load(url)
+        .placeholder(R.drawable.ic_person_24dp)
+        .fit()
         .into(this)
 }
