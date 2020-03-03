b79f17fbe4ede68bc17bbc050c5eaf80b9a25ed0
Stanislav <stas.elovsky@gmail.com>
2020-03-03 13:42:09 +0200

    dayscheduleviewmodel 2

--- app/src/main/java/com/helicopter/ui/fragments/schedule/day/DayScheduleViewModel.kt
+++ app/src/main/java/com/helicopter/ui/fragments/schedule/day/DayScheduleViewModel.kt
@@ -1,4 +1,23 @@
 package com.helicopter.ui.fragments.schedule.day
 
-class DayScheduleViewModel {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.helicopter.data.database.database.getInstance
+import com.helicopter.data.repository.schedule.ScheduleRepositoryImpl
+
+class DayScheduleViewModel(app: Application): ViewModel() {
+
+    private val database = getInstance(app)
+    private val repository = ScheduleRepositoryImpl(database)
+
+    class Factory(private val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DayScheduleViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DayScheduleViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
