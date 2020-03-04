29b31400750239e005c3ac48e09c669a0c08ea01
Stanislav <stas.elovsky@gmail.com>
2020-03-04 11:41:29 +0300

    subgroup info

---
+++ app/src/main/java/com/helicopter/ui/bindingUtils/TextViewUtils.kt
@@ -0,0 +1,13 @@
+package com.helicopter.ui.bindingUtils
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.helicopter.R
+
+@BindingAdapter("subgroup")
+fun TextView.setSubgroupText(subgroup: Int) {
+    text = when (subgroup){
+        0 -> ""
+        else -> context.resources.getString(R.string.subgroup, subgroup)
+    }
+}
